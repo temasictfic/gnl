5ee40554267c55b9d0c4989ee9a8b16e9cec0ee9
Samet Ciftci <sciftci@k2m18s01.42kocaeli.com.tr>
2023-01-04 12:41:42 +0300

    multiple fd main

--- main.c
+++ main.c
@@ -4,17 +4,28 @@
 
 int	main(int argc, char **argv)
 {
-	char	*line;
-	int		fd;
+	char	*line1;
+	char	*line2;
+	int		fd1;
+	int 	fd2;
 
 	(void)argc;
-	fd = open(argv[1], O_RDONLY);
-	line = get_next_line(fd);
-	while (line != NULL)
+	fd1 = open(argv[1], O_RDONLY);
+	fd2	= open(argv[2], O_RDONLY);
+	line1 = get_next_line(fd1);
+	line2 = get_next_line(fd2);
+	while (line1 != NULL)
 	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
+		printf("line1: %s", line1);
+		free(line1);
+		line1 = get_next_line(fd1);
+	}
+	printf("\n\n");
+	while (line2 != NULL)
+	{
+		printf("line2: %s", line2);
+		free(line2);
+		line2 = get_next_line(fd2);
 	}
 	return (0);
 }
